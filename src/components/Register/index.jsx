05e6b666e4f456b3a9c7b4f83be95ec0f27e1122
DiogoBozan <diogo_fb_@hotmail.com>
2022-06-10 20:25:13 +0100

    created index inside of components

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,18 +1,1 @@
-import { Link } from "react-router-dom";
-
-function Register() {
-  return (
-    <div>
-      <p>Register Page</p>
-      <input type="text" placeholder="Name" />
-      <input type="email" placeholder="E-mail" />
-      <input type="password" placeholder="Password" />
-      <button type="button">Register</button>
-      <div>
-        <Link to="/">Login</Link>{" "}
-      </div>
-    </div>
-  );
-}
-
-export default Register;
+export { default } from "./Register";
